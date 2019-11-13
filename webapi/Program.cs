631dae157bf216374219ef4f303ce540ca340d72
yellow-strelitzia <53438992+yellow-strelitzia@users.noreply.github.com>
2019-11-13 17:54:10 +0900

    Update Program.cs

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -16,11 +16,23 @@ namespace webapi
             CreateHostBuilder(args).Build().Run();
         }
 
+        /*
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        */
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:" + port);
+                });
+        }
     }
 }
